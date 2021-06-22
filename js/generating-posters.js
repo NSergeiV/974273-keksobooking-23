8e3b9d199a99b9ceb19c593257f7bebbd9d3b4b6
NSergeiV <ttniseva@yandex.ru>
2021-06-23 00:30:35 +0300

    Основную часть закончил задания 7

--- js/generating-posters.js
+++ js/generating-posters.js
@@ -1,7 +1,3 @@
-// import {objects} from './main.js';
-// console.log(objects);
-
-/*
 const fragment = document.createDocumentFragment();
 const template = document.querySelector('#card').content.querySelector('.popup');
 const mapCanvas = document.querySelector('#map-canvas');
@@ -21,60 +17,40 @@ const gettingTypeHousing = (type) => {
       return 'Непонятно!';
   }
 };
-const collectionAds = objects;
+// const collectionAds = objects;
 
-collectionAds.forEach((ad) => {
-    const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-  let newElement = template.cloneNode(true);
-  newElement.querySelector('.popup__avatar').src = author.avatar;
-  newElement.querySelector('.popup__title').textContent = offer.title;
-  newElement.querySelector('.popup__text--address').textContent = offer.address;
-  newElement.querySelector('.popup__text--price').querySelector('span').insertAdjacentHTML('beforebegin', `${offer.price} `);
-  newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(offer.type)}`;
-  newElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  newElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}}`;
-  newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
-    const classItem = item.classList[1];
-    if (!modifiers.includes(classList)) {
-      item.remove();
+const generatingPosters = (ad) => {
+  for (let i = 0; i < 1; i++) {
+    const conveniences = ad.offer.features;
+    const modifiers = conveniences.map((feature) => `popup__feature--${feature}`);
+    const newElement = template.cloneNode(true);
+    newElement.querySelector('.popup__avatar').src = ad.author.avatar;
+    newElement.querySelector('.popup__title').textContent = ad.offer.title;
+    newElement.querySelector('.popup__text--address').textContent = ad.offer.address;
+    newElement.querySelector('.popup__text--price').textContent = ' ';
+    newElement.querySelector('.popup__text--price').insertAdjacentHTML('afterbegin', `${ad.offer.price} <span>₽/ночь</span>`);
+    newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(ad.offer.type)}`;
+    newElement.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+    newElement.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}}`;
+    newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
+      const classItem = item.classList[1];
+      if (!modifiers.includes(classItem)) {
+        item.remove();
+      }
+    });
+    newElement.querySelector('.popup__description').textContent = ad.offer.description;
+    const blockPhotos = newElement.querySelector('.popup__photos');
+    blockPhotos.querySelector('img').remove();
+    for (let j = 0; j < ad.offer.photos.length; j++) {
+      blockPhotos.insertAdjacentHTML('beforeend', `<img src="${ad.offer.photos[j]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
     }
-  });
-  newElement.querySelector('.popup__description').textContent = offer.description;
-  const blockPhotos = newElement.querySelector('.popup__photos');
-  for (let i = 0; i < offer.photos.length; i++) {
-    blockPhotos.insertAdjacentElement('beforeend', `<img src="${offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
-  }
 
-  newElement.querySelector('.popup__text--address').textContent = 'ДОБАВИЛ';
-  fragment.appendChild(newElement);
-});
-*/
-/*
-for (i = 0; i < 1; i++) {
-  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-  let newElement = template.cloneNode(true);
-  newElement.querySelector('.popup__avatar').src = author.avatar;
-  newElement.querySelector('.popup__title').textContent = offer.title;
-  newElement.querySelector('.popup__text--address').textContent = offer.address;
-  newElement.querySelector('.popup__text--price').querySelector('span').insertAdjacentHTML('beforebegin', `${offer.price} `);
-  newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(offer.type)}`;
-  newElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  newElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}}`;
-  newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
-    const classItem = item.classList[1];
-    if (!modifiers.includes(classList)) {
-      item.remove();
-    }
-  });
-  newElement.querySelector('.popup__description').textContent = offer.description;
-  const blockPhotos = newElement.querySelector('.popup__photos');
-  for (let i = 0; i < offer.photos.length; i++) {
-    blockPhotos.insertAdjacentElement('beforeend', `<img src="${offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+    fragment.appendChild(newElement);
   }
+  mapCanvas.appendChild(fragment);
+};
+// console.log(fragment);
 
-  newElement.querySelector('.popup__text--address').textContent = 'ДОБАВИЛ';
-  fragment.appendChild(newElement);
-}
-*/
 // mapCanvas.appendChild(fragment);
 
+export {generatingPosters};
--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 import {fillingObject} from './data.js';
+import {generatingPosters} from './generating-posters.js';
 
 const SIMILAR_DATA = 10;
 
 const objects = new Array(SIMILAR_DATA).fill(null).map(() => fillingObject());
 
-export {objects};
+// export {objects};
 
-/* console.log(objects);
+generatingPosters(objects[0]);
 
 // export {objects};
 
 //import {objects} from './main.js';
 //console.log(objects);
-
+/*
 const fragment = document.createDocumentFragment();
 const template = document.querySelector('#card').content.querySelector('.popup');
 const mapCanvas = document.querySelector('#map-canvas');
@@ -35,32 +36,30 @@ const gettingTypeHousing = (type) => {
 const collectionAds = objects;
 
 collectionAds.forEach((ad) => {
-  console.log(ad);
-  console.log(ad.offer.features);
   const conveniences = ad.offer.features;
-  console.log(conveniences);
   const modifiers = conveniences.map((feature) => `popup__feature--${feature}`);
   let newElement = template.cloneNode(true);
   newElement.querySelector('.popup__avatar').src = ad.author.avatar;
   newElement.querySelector('.popup__title').textContent = ad.offer.title;
   newElement.querySelector('.popup__text--address').textContent = ad.offer.address;
-  newElement.querySelector('.popup__text--price').querySelector('span').insertAdjacentHTML('beforebegin', `${ad.offer.price} `);
+  newElement.querySelector('.popup__text--price').textContent = ' ';
+  newElement.querySelector('.popup__text--price').insertAdjacentHTML('afterbegin', `${ad.offer.price} <span>₽/ночь</span>`)
   newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(ad.offer.type)}`;
   newElement.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
-  newElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}}`;
+  newElement.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}}`;
   newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
     const classItem = item.classList[1];
-    if (!modifiers.includes(classList)) {
+    if (!modifiers.includes(classItem)) {
       item.remove();
     }
   });
-  newElement.querySelector('.popup__description').textContent = offer.description;
+  newElement.querySelector('.popup__description').textContent = ad.offer.description;
   const blockPhotos = newElement.querySelector('.popup__photos');
-  for (let i = 0; i < offer.photos.length; i++) {
-    blockPhotos.insertAdjacentElement('beforeend', `<img src="${offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+  blockPhotos.querySelector('img').remove();
+  for (let i = 0; i < ad.offer.photos.length; i++) {
+    blockPhotos.insertAdjacentHTML('beforeend', `<img src="${ad.offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
   }
 
-  newElement.querySelector('.popup__text--address').textContent = 'ДОБАВИЛ';
   fragment.appendChild(newElement);
 });
 
