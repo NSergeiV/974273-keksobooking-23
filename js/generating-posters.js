46d62582f9f824f5eb75a4d2701087755b356be6
NSergeiV <ttniseva@yandex.ru>
2021-06-22 18:42:27 +0300

    Отправил

--- js/generating-posters.js
+++ js/generating-posters.js
@@ -1,4 +1,6 @@
 // import {objects} from './main.js';
+// console.log(objects);
+
 /*
 const fragment = document.createDocumentFragment();
 const template = document.querySelector('#card').content.querySelector('.popup');
@@ -19,11 +21,10 @@ const gettingTypeHousing = (type) => {
       return 'Непонятно!';
   }
 };
-// const collectionAds = objects;
-// collectionAds.forEach((ad) => {});
+const collectionAds = objects;
 
-
-for (i = 0; i < 1; i++) {
+collectionAds.forEach((ad) => {
+    const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
   let newElement = template.cloneNode(true);
   newElement.querySelector('.popup__avatar').src = author.avatar;
   newElement.querySelector('.popup__title').textContent = offer.title;
@@ -32,9 +33,48 @@ for (i = 0; i < 1; i++) {
   newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(offer.type)}`;
   newElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   newElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}}`;
+  newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
+    const classItem = item.classList[1];
+    if (!modifiers.includes(classList)) {
+      item.remove();
+    }
+  });
+  newElement.querySelector('.popup__description').textContent = offer.description;
+  const blockPhotos = newElement.querySelector('.popup__photos');
+  for (let i = 0; i < offer.photos.length; i++) {
+    blockPhotos.insertAdjacentElement('beforeend', `<img src="${offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+  }
+
+  newElement.querySelector('.popup__text--address').textContent = 'ДОБАВИЛ';
+  fragment.appendChild(newElement);
+});
+*/
+/*
+for (i = 0; i < 1; i++) {
+  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+  let newElement = template.cloneNode(true);
+  newElement.querySelector('.popup__avatar').src = author.avatar;
+  newElement.querySelector('.popup__title').textContent = offer.title;
+  newElement.querySelector('.popup__text--address').textContent = offer.address;
+  newElement.querySelector('.popup__text--price').querySelector('span').insertAdjacentHTML('beforebegin', `${offer.price} `);
+  newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(offer.type)}`;
+  newElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  newElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}}`;
+  newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
+    const classItem = item.classList[1];
+    if (!modifiers.includes(classList)) {
+      item.remove();
+    }
+  });
+  newElement.querySelector('.popup__description').textContent = offer.description;
+  const blockPhotos = newElement.querySelector('.popup__photos');
+  for (let i = 0; i < offer.photos.length; i++) {
+    blockPhotos.insertAdjacentElement('beforeend', `<img src="${offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+  }
 
   newElement.querySelector('.popup__text--address').textContent = 'ДОБАВИЛ';
   fragment.appendChild(newElement);
 }
-mapCanvas.appendChild(fragment);
 */
+// mapCanvas.appendChild(fragment);
+
--- js/main.js
+++ js/main.js
@@ -4,6 +4,65 @@ const SIMILAR_DATA = 10;
 
 const objects = new Array(SIMILAR_DATA).fill(null).map(() => fillingObject());
 
-objects;
+export {objects};
+
+/* console.log(objects);
 
 // export {objects};
+
+//import {objects} from './main.js';
+//console.log(objects);
+
+const fragment = document.createDocumentFragment();
+const template = document.querySelector('#card').content.querySelector('.popup');
+const mapCanvas = document.querySelector('#map-canvas');
+const gettingTypeHousing = (type) => {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+    case 'hotel':
+      return 'Отель';
+    default:
+      return 'Непонятно!';
+  }
+};
+const collectionAds = objects;
+
+collectionAds.forEach((ad) => {
+  console.log(ad);
+  console.log(ad.offer.features);
+  const conveniences = ad.offer.features;
+  console.log(conveniences);
+  const modifiers = conveniences.map((feature) => `popup__feature--${feature}`);
+  let newElement = template.cloneNode(true);
+  newElement.querySelector('.popup__avatar').src = ad.author.avatar;
+  newElement.querySelector('.popup__title').textContent = ad.offer.title;
+  newElement.querySelector('.popup__text--address').textContent = ad.offer.address;
+  newElement.querySelector('.popup__text--price').querySelector('span').insertAdjacentHTML('beforebegin', `${ad.offer.price} `);
+  newElement.querySelector('.popup__type').textContent = `${gettingTypeHousing(ad.offer.type)}`;
+  newElement.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  newElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}}`;
+  newElement.querySelector('.popup__features').querySelectorAll('.popup__feature').forEach((item) => {
+    const classItem = item.classList[1];
+    if (!modifiers.includes(classList)) {
+      item.remove();
+    }
+  });
+  newElement.querySelector('.popup__description').textContent = offer.description;
+  const blockPhotos = newElement.querySelector('.popup__photos');
+  for (let i = 0; i < offer.photos.length; i++) {
+    blockPhotos.insertAdjacentElement('beforeend', `<img src="${offer.photos[i]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`);
+  }
+
+  newElement.querySelector('.popup__text--address').textContent = 'ДОБАВИЛ';
+  fragment.appendChild(newElement);
+});
+
+mapCanvas.appendChild(fragment);
+*/
