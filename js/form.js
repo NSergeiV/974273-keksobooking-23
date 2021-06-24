1bb37ea51a65ca03a39c3a1eeb8039a14dda6dce
NSergeiV <ttniseva@yandex.ru>
2021-06-24 23:25:06 +0300

    Сделал дополнительное задание - 7.10. Отрисуй меня полностью (часть 2)

--- js/form.js
+++ js/form.js
@@ -2,14 +2,27 @@ const adForm = document.querySelector('.ad-form');
 const mapFilter = document.querySelector('.map__filters');
 
 const adFormChildren = adForm.querySelectorAll('fieldset');
-const mapFilterChildren = mapFilter.children;
+const mapFilterChildren = mapFilter.querySelectorAll('.map__filter');
 
-const blockIt = (block, children) => {
+const blockIt = (block, listChildren) => {
   block.classList.add('ad-form--disabled');
-  children.forEach((item) => {
+  listChildren.forEach((item) => {
     item.setAttribute('disabled', '');
   });
 };
 
 blockIt(adForm, adFormChildren);
 blockIt(mapFilter, mapFilterChildren);
+
+const unlock = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilter.classList.remove('ad-form--disabled');
+  adFormChildren.forEach((item) => {
+    item.removeAttribute('disabled');
+  });
+  mapFilterChildren.forEach((item) => {
+    item.removeAttribute('disabled');
+  });
+};
+
+unlock();
