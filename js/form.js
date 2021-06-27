c4ba1cf710f1c7ad7ea672e337bbbeca6e982bca
NSergeiV <ttniseva@yandex.ru>
2021-06-28 00:26:46 +0300

    Приступил к заданию 8.10. Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -26,3 +26,34 @@ const unlock = () => {
 };
 
 unlock();
+
+// Валидация Формы подачи объявления
+const inputTitle = adForm.querySelector('#title');
+const inputPrice = adForm.querySelector('#price');
+const inputType = adForm.querySelector('#type');
+const MIN_HEADING = 30;
+const MAX_HEADING = 100;
+const MAX_PRICE = 1000000;
+
+inputTitle.addEventListener('input', (evt) => {
+  const valueLength = inputTitle.value.length;
+  if (valueLength < MIN_HEADING) {
+    inputTitle.setCustomValidity(`Не хватает ${MIN_HEADING - valueLength} символ`);
+  } else if (valueLength > MAX_HEADING) {
+    inputTitle.setCustomValidity(`Лишний ${valueLength - MAX_HEADING} символ`);
+  } else {
+    inputTitle.setCustomValidity('');
+  }
+
+  inputTitle.reportValidity();
+});
+inputPrice.addEventListener('input', (evt) => {
+  const valuePrice = inputPrice.value;
+  if (valuePrice > MAX_PRICE) {
+    inputPrice.setCustomValidity(`Значение должно быть меньше или равно ${MAX_PRICE}`);
+  } else {
+    inputPrice.setCustomValidity('');
+  }
+
+  inputPrice.reportValidity();
+});
